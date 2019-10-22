fadf4d59cac331833c8b729a545e7b865120adbb
czk <1052031630@qq.com>
2019-10-23 00:29:09 +0800

    爬虫项目的添加

--- 51job/src/main/java/cn/itcast/task/CornTest.java
+++ 51job/src/main/java/cn/itcast/task/CornTest.java
@@ -1,12 +1,13 @@
-package cn.itcast.task;
+/*package cn.itcast.task;
 
+import org.graalvm.compiler.lir.CompositeValue;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-@Component
+
+
 public class CornTest {
     @Scheduled(cron = "0/5 * * * * *")
     public void corn(){
         System.out.println("dandan");
     }
-}
+}*/
